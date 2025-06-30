98f6159c8ebed0230c5dce791645de035c6c4c40
Marcelo <DEV.marceloakutsu@gmail.com>
2025-06-30 16:49:42 +0900

    Initial Commit

---
+++ StudyAPIOpen-Meteo/StudyAPIOpen_MeteoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyAPIOpen_MeteoApp.swift
+//  StudyAPIOpen-Meteo
+//
+//  Created by AMTK on 2025/06/30.
+//
+
+import SwiftUI
+
+@main
+struct StudyAPIOpen_MeteoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StudyAPIOpen-MeteoTests/StudyAPIOpen_MeteoTests.swift
@@ -0,0 +1,17 @@
+//
+//  StudyAPIOpen_MeteoTests.swift
+//  StudyAPIOpen-MeteoTests
+//
+//  Created by AMTK on 2025/06/30.
+//
+
+import Testing
+@testable import StudyAPIOpen_Meteo
+
+struct StudyAPIOpen_MeteoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

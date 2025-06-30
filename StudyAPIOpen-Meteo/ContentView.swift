5d8b86e911fadd4348c1207f05c75f853d4f232e
Marcelo <DEV.marceloakutsu@gmail.com>
2025-06-30 17:54:41 +0900

    first commit

--- StudyAPIOpen-Meteo/ContentView.swift
+++ StudyAPIOpen-Meteo/ContentView.swift
@@ -6,16 +6,58 @@
 //
 
 import SwiftUI
+//codable is available from the json
+struct WeatherData: Codable {
+    let current: CurrentWeather
+}
+
+struct CurrentWeather: Codable {
+    let temperature_2m: Double
+    let wind_speed_10m: Double
+}
 
 struct ContentView: View {
+    @State private var temperature: Double?
+    @State private var windSpeed: Double?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text("Current Weather")
+                    .font(.largeTitle)
+                    .padding()
+                
+                if let temperature = temperature {
+                    Text("Temperature: \(temperature) C")
+                        .font(.title2)
+                }
+                if let windSpeed = windSpeed {
+                    Text("Wind Speed \(windSpeed) km/h")
+                }
+            }
+            .padding()
+            .task {
+                await fetchWeather()
+            }
+            .navigationTitle("Weather")
         }
-        .padding()
+    }
+    
+    func fetchWeather() async {
+        let urlString = "https://api.open-meteo.com/v1/forecast?latitude=34.62&longitude=136.50&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
+        guard let url = URL(string: urlString) else { return }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let decodedData = try JSONDecoder().decode(WeatherData.self, from: data)
+            
+            DispatchQueue.main.async {
+                self.temperature = decodedData.current.temperature_2m
+                self.windSpeed = decodedData.current.wind_speed_10m
+            }
+        } catch {
+            print("error fetching weather data \(error)")
+        }
     }
 }
 
